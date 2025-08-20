0c0f3604b731343b66c4f68b7f74f246a89e9c57
aravk <aravk@altair.com>
2025-08-20 14:55:18 +0530

    Added timemachine

---
+++ lua/plugins/timemachine.lua
@@ -0,0 +1,11 @@
+return {
+	"y3owk1n/time-machine.nvim",
+	cmd = {
+		"TimeMachineToggle",
+		"TimeMachinePurgeBuffer",
+		"TimeMachinePurgeAll",
+		"TimeMachineLogShow",
+		"TimeMachineLogClear",
+	},
+	config = true,
+}
