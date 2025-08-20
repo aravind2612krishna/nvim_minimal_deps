f09c5891bed90b156f943bfefb00e40e8eab102f
aravk <aravk@altair.com>
2025-08-20 14:31:21 +0530

    Fix to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,23 +8,25 @@ return {
 		local mason = require("mason")
 
 		mason.setup()
-		vim.lsp.config("clangd", {
-			cmd = {
-				"clangd",
-				"--background-index",
-				"-j=10",
-				"--clang-tidy",
-				"--enable-config",
-				"--header-insertion=never",
-				"--completion-style=detailed",
-				"--function-arg-placeholders",
-				"--include-ineligible-results",
-			},
-			init_options = {
-				usePlaceholders = true,
-				completeUnimported = true,
-				clangdFileStatus = true,
-			},
-		})
+		if vim.fn.has("nvim-0.12") == 1 then
+			vim.lsp.config("clangd", {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"-j=10",
+					"--clang-tidy",
+					"--enable-config",
+					"--header-insertion=never",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--include-ineligible-results",
+				},
+				init_options = {
+					usePlaceholders = true,
+					completeUnimported = true,
+					clangdFileStatus = true,
+				},
+			})
+		end
 	end,
 }
