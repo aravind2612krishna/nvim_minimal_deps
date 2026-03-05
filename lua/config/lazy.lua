2a569a20b09656749fa5014284ceb44135701d89
aravk <aravk@altair.com>
2026-03-05 10:41:40 +0530

    Remove opacity stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -247,26 +247,10 @@ if vim.g.neovide then
         change_scale_factor(1 / 1.05)
     end)
 
-    -- Helper function for transparency formatting
-    local alpha = function()
-        return string.format("%x", math.floor(255 * vim.g.neovide_opacity_point or 0.8))
-    end
     -- Set transparency and background color (title bar color)
     vim.g.neovide_opacity = 1.0
     -- vim.g.neovide_opacity_point = 0.8
-    vim.g.neovide_background_color = "#0f1117" .. alpha()
-    -- Add keybinds to change transparency
-    local change_transparency = function(delta)
-        vim.g.neovide_opacity_point = vim.g.neovide_opacity_point + delta
-        vim.g.neovide_background_color = "#0f1117" .. alpha()
-    end
-    vim.keymap.set({ "n", "v", "o" }, "<M-]>", function()
-        change_transparency(0.01)
-    end)
-    vim.keymap.set({ "n", "v", "o" }, "<M-[>", function()
-        change_transparency(-0.01)
-    end)
-
+    -- vim.g.neovide_background_color = "#0f1117" .. alpha()
     vim.g.neovide_floating_corner_radius = 0.5
 
     vim.g.neovide_floating_blur_amount_x = 2.0
